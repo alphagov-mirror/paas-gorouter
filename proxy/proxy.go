3ade9e20f1088ffbffa63c04abf126d22a189567
Lee Porte <lee.porte@digital.cabinet-office.gov.uk>
2017-11-08 11:54:21 +0000

    Find Client IP for use in x-real-ip header

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -10,6 +10,7 @@ import (
 	"sync"
 	"time"
 
+	"bytes"
 	"code.cloudfoundry.org/gorouter/access_log"
 	router_http "code.cloudfoundry.org/gorouter/common/http"
 	"code.cloudfoundry.org/gorouter/config"
@@ -262,6 +263,71 @@ func (p *proxy) ServeHTTP(responseWriter http.ResponseWriter, request *http.Requ
 	next(responseWriter, request)
 }
 
+type ipRange struct {
+	start net.IP
+	end   net.IP
+}
+
+func inRange(r ipRange, ipAddress net.IP) bool {
+
+	if bytes.Compare(ipAddress, r.start) >= 0 && bytes.Compare(ipAddress, r.end) < 0 {
+		return true
+	}
+	return false
+}
+
+var privateRanges = []ipRange{
+	ipRange{
+		start: net.ParseIP("10.0.0.0"),
+		end:   net.ParseIP("10.255.255.255"),
+	},
+	ipRange{
+		start: net.ParseIP("100.64.0.0"),
+		end:   net.ParseIP("100.127.255.255"),
+	},
+	ipRange{
+		start: net.ParseIP("172.16.0.0"),
+		end:   net.ParseIP("172.31.255.255"),
+	},
+	ipRange{
+		start: net.ParseIP("192.0.0.0"),
+		end:   net.ParseIP("192.0.0.255"),
+	},
+	ipRange{
+		start: net.ParseIP("192.168.0.0"),
+		end:   net.ParseIP("192.168.255.255"),
+	},
+	ipRange{
+		start: net.ParseIP("198.18.0.0"),
+		end:   net.ParseIP("198.19.255.255"),
+	},
+}
+
+func isPrivateSubnet(ipAddress net.IP) bool {
+	if ipCheck := ipAddress.To4(); ipCheck != nil {
+		for _, r := range privateRanges {
+			if inRange(r, ipAddress) {
+				return true
+			}
+		}
+	}
+	return false
+}
+func GetIPAddress(r *http.Request) string {
+	for _, h := range []string{"X-Forwarded-For", "X-Real-Ip"} {
+		addresses := strings.Split(r.Header.Get(h), ",")
+		for i := len(addresses) - 1; i >= 0; i-- {
+			RealIP := strings.TrimSpace(addresses[i])
+			realIP := net.ParseIP(RealIP)
+			if !realIP.IsGlobalUnicast() || isPrivateSubnet(realIP) {
+				continue
+			}
+			return RealIP
+		}
+	}
+	return ""
+}
+
 func (p *proxy) setupProxyRequest(target *http.Request) {
 	if p.forceForwardedProtoHttps {
 		target.Header.Set("X-Forwarded-Proto", "https")
@@ -286,8 +352,8 @@ func (p *proxy) setupProxyRequest(target *http.Request) {
 	target.URL.ForceQuery = false
 	target.URL.Opaque = target.RequestURI
 
-	if clientIP, _, err := net.SplitHostPort(target.RemoteAddr); err == nil {
-		target.Header.Set("X-Real-IP", clientIP)
+	if remoteIP := GetIPAddress(target); err == nil {
+		target.Header.Set("X-Real-IP", remoteIP)
 	}
 
 	if strings.HasPrefix(target.RequestURI, "//") {
